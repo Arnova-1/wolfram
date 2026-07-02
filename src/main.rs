25b1640d29fba2a865c3e1736c6987285032f7b6
Arnova-1 <arnova124@gmail.com>
2026-07-02 14:33:55 +0700

    read keypresses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut buffer = [0u8; 1];
+
+    while io::stdin().read(&mut buffer)? == 1 && buffer != [b'q'] {};
+
+    Ok(())
 }
